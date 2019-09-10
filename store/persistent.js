fb95ccd67cbbbf87e64262e089118275216ea4f7
goblin-pitcher <51311765+goblin-pitcher@users.noreply.github.com>
2019-09-10 22:33:12 +0800

    Update persistent.js

--- store/persistent.js
+++ store/persistent.js
@@ -1,5 +1,5 @@
 import {
-	observer,
+	persistedState,
 	getType,
 	getStoragePath,
 	setMutations
@@ -37,26 +37,4 @@ export default {
 以下是对于持久化参数的处理，当此文件state中的值改变时，自动更新对应项的持久化存储
  */
 
-function debounceStorage(delay) {
-	let updateItems = new Set()
-	let timer = null
-	return function setToStorage(obj, key) {
-		let changeKey = getStoragePath(obj, key)[0]
-		updateItems.add(changeKey)
-		clearTimeout(timer)
-		timer = setTimeout(() => {
-			try {
-				updateItems.forEach(key => {
-					uni.setStorageSync(key, state[key])
-				})
-				updateItems.clear()
-			} catch (e) {
-				console.error(`persistent.js中state内容持久化失败,错误位于[${changeKey}]参数中的[${key}]项`)
-			}
-		}, delay)
-	}
-}
-
-observer(state, {
-	set: debounceStorage(1000) //1000仅是测试值，实际可设为200以内或直接设为0
-})
+persistedState({state, setItem: uni.setStorageSync, setDelay: 1000})
